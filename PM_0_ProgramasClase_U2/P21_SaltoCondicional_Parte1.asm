8e7600fc2d60f9f5598d2f7ae4c47950029b847b
ahgarciar <ahgarcia@docentes.uat.edu.mx>
2021-09-21 13:00:12 -0500

    Clase 21 Sept

--- PM_0_ProgramasClase_U2/P21_SaltoCondicional_Parte1.asm
+++ PM_0_ProgramasClase_U2/P21_SaltoCondicional_Parte1.asm
@@ -30,14 +30,19 @@ INCLUDE Irvine32.inc
 		; DEBE REVISARSE EL REGISTRO DE BANDERAS PARA COMPROBAR SI SE CUMPLE O NO UNA CONDICIÓN
 
 		MOV EAX, 10
+		call dumpregs
 
 		CMP EAX, 12  ; "COMPARA" SI DOS OPERANDOS SON IGUALES O SI UNO ES MAYOR O MENOR QUE EL OTRO
 					 ; LA COMPARACIÓN SE REALIZA APLICANDO UNA RESTA ENTRE LOS OPERANDOS, QUE NO 
 					 ; MODIFICA LOS OPERANDOS, SIN EMBARGO, SI MODIFICA A LAS BANDERAS
+		call dumpregs
+
 
 		CMP EAX, 10
+		call dumpregs
 
 		CMP EAX, 5
+		call dumpregs
 
 
 		exit	
@@ -53,4 +58,10 @@ INCLUDE Irvine32.inc
 	;Programas: nombre 
 
 	;Unidad 2:
-	;Practica 1:  4 programas en documentacion (pendiente) / video (asignacion)
+	;Tarea 1 (Practica 1):  4 programas en documentacion (pendiente) / video (asignacion)
+
+	;Tarea 2. Registro de Banderas   y las banderas más comunes
+	
+	;Tarea 3. Saltos Condicionales con Signo
+
+	;Tarea 4. Saltos Condicionales sin Signo
