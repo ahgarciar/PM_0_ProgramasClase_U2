3a98e61e875be13dfee4ffffedaf3884de3b60dc
ahgarciar <ahgarcia@docentes.uat.edu.mx>
2021-09-28 09:02:39 -0500

    28 de Sept

---
+++ PM_0_ProgramasClase_U2/P_30_LOOP_.asm
@@ -0,0 +1,43 @@
+TITLE instruccion LOOP
+
+;DESCRPICIÓN 
+;Objetivo: Explicación
+;
+; Autore(s):
+;         Mtro. Alejandro Garcia
+;		  Su Nombre.! 
+;         
+
+; Semestre: 8vo Semestre ISC 
+;FIN DESCRIPCIÓN
+
+INCLUDE Irvine32.inc  
+
+.data
+; Área de Declaración de Variables
+
+.code
+	
+	main30 PROC	
+		;Lógica del Programa
+
+		;LOOP Es una intruscción que nos permite generar ciclos, como si de un 
+		; ciclo FOR se tratará.
+
+		; En esta instrucción siempre se corroborá (compara) ECX y se decrementa 
+		; de forma automática
+
+		mov ecx, 10
+
+		ciclo:
+			mov eax, ecx
+			call writedec
+			call crlf
+
+			loop ciclo
+
+		exit	
+	
+	main30 ENDP
+	
+	END main30
